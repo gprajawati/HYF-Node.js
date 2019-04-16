5006dfca40317fda16507fbc75b8fa7a05d99e48
itsmegith <bettercallgita@gmail.com>
2019-04-16 23:25:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const lodash = require('lodash');
+
+/*Create an array of objects {name: “”, age: “”}*/
+let person = [
+
+    {
+        name: "Jon Snow",
+        age: 38
+    },
+    {
+        name: "Daenerys Targaryen",
+        age: 30
+    },
+    {
+        name: "Arya Stark",
+        age: 28
+    },
+    {
+        name: "Tyrion Lannister",
+        age: 44
+    }
+]
+
+let people = lodash.map(person, (myPeople) => {
+    console.log(myPeople)
+})
+
+
+/*Sort the array in a descending order based on the age. lodash*/
+const descAge = lodash.orderBy(person, ['age'], ['desc'])
+console.log(descAge)
+
+
+/*Find the oldest person. lodash*/
+let oldestPerson = lodash.maxBy(person, (maxAge) => {
+    return maxAge.age
+})
+console.log(oldestPerson)
+
+
+/*Find the youngest person. lodash*/
+let youngestPerson = lodash.minBy(person, (minAge) => {
+    return minAge.age
+})
+console.log(youngestPerson)
+
+
+/*Remove the oldest person from the array. lodash*/
+const removeTheOldest = lodash.remove(person, (removeOldPerson) => {
+    return removeOldPerson === oldestPerson;
+});
+console.log(removeTheOldest)
+console.log(person)
+
+
+/*Add some objects to the array with the same age but different names*/
+const addPeople = person.push({ name: 'Joffrey Baratheon', age: 25 }, { name: 'Bran Stark', age: 25 }, { name: 'Khal Drogo', age: 40 }, {name: 'Theon Greyjoy', age: 30}, {name: 'Jaime Lannister', age: 40});
+console.log(addPeople[person])
+console.log(person)
+
+
+/*Group the people by age. lodash*/
+const groupedByAge = lodash.groupBy(person, person => person.age);
+console.log(groupedByAge)
+
+
+
+/* for (let i = 0; i < person.length; i++) {
+     fs.writeFile('people.txt', `${person[i].name} is ${person[i].age}`, (err) => {
+         if (err) {
+             console.log(err)
+             return;
+         }
+         //console.log(allPeople)
+     })
+ }*/
+
+
+
+const addToFile = () => {
+    for (let i = 0; i < person.length; i++) {
+        fs.writeFile('people.txt', `${person[i].name} is ${person[i].age}`, (err) => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            console.log('File Saved')
+        })
+        console.log(`${person[i].name} is ${person[i].age}`);
+    }
+}
+
+/*fs.writeFile('people.txt', 'Hey you', (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('File saved');
+})*/
